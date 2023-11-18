4515318c8c143a113aec42a0f6f4f42869cd73ea
RafaelLima12 <142914994+RafaelLima12@users.noreply.github.com>
2023-11-17 22:23:48 -0300

    Add files via upload

---
+++ TPA/Lista15Vetores/Palindromo.java
@@ -0,0 +1,36 @@
+//exercicio 12
+
+import java.util.Scanner;
+
+public class Palindromo {
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int a[] = new int [10];
+		
+		for (int i = 0; i<a.length; i++) {
+			System.out.println("Digite o "+(i+1)+"° número");
+			a[i] = ler.nextInt();
+		}
+		
+		int b = 0;
+		
+		int j = 9;
+		for(int i = 0; i<a.length; i++) {
+			if(a[i] == a[j]) {
+				b = b+1;
+			}
+			
+			j--;
+		}
+		
+		if(b==10) {
+			System.out.println("O número é um palindromo");
+		}else {
+			System.out.println("O número não é um palindromo");
+		}
+
+	}
+
+}
+
