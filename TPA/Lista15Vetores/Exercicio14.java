4cc3072b5f7e8890bbf8b70a2895d2bbd781b78e
RafaelLima12 <142914994+RafaelLima12@users.noreply.github.com>
2023-11-19 20:14:49 -0300

    Add files via upload

---
+++ TPA/Lista15Vetores/Exercicio14.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+public class Exercicio14 {
+
+	public static void main(String[] args) {
+		Scanner ler = new Scanner(System.in);
+		
+		int a[] = new int [10];
+		
+		int b[] = new int [10];
+		
+		int c[] = new int [10];
+		
+		for(int i = 0; i<a.length; i++) {
+			System.out.println("Digite o "+(i+1)+"° valor de A");
+			a[i] = ler.nextInt();
+		}
+		
+		for(int i = 0; i<a.length; i++) {
+			System.out.println("Digite o "+(i+1)+"° valor de B");
+			b[i] = ler.nextInt();
+		}
+		
+		for(int i = 0; i<a.length; i++) {
+			if(a[i]>b[i]) {
+				c[i] = 1;
+			}else if(a[i] == b[i]) {
+				c[i] = 0;
+			}else {
+				c[i] = -1;
+			}
+		}
+		
+		System.out.print("\nC[ ");
+		for(int i = 0; i<a.length; i++) {
+			System.out.print(c[i]+" ");
+		}
+		System.out.print("]");
+	}
+}
